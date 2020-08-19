d908f1724812e6599d586ac50a092f544d54ef95
OscarSanchezM97 <oscarsanchezmeza97@gmail.com>
2020-08-19 11:10:51 -0600

    animations with css

--- js/animation.js
+++ js/animation.js
@@ -1,19 +1,11 @@
 'use strict'
-let animationSidebar1 = document.getElementById("cv").addEventListener('mouseover', sidebarAppear);
-let animationSidebar2 = document.getElementById("contact").addEventListener('mouseover', sidebarAppear);
-let btnContact = document.getElementById("btn-contact").addEventListener('click', contactAppears);
+document.querySelector('#btn-contact').addEventListener('click', contactAppears);
 
 let btnSend = document.querySelector("#btnSend");
-btnSend.addEventListener('click', ()=>{
-    contact();
-});
+btnSend.addEventListener('click', contact)
 
 
-function sidebarAppear(){
-    var side = document.getElementById("sidebar");
-    side.style.display = "block";
-    side.style.transition = "3";
-}
+
 function contactAppears(){
     var contact = document.getElementById("contact");
     contact.style.display= "block";
